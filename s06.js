c259ef7b44554ac832b5a6575434ae4a382a4d7f
Morteza Sahami <sahamimorteza72@gmail.com>
2023-11-07 17:30:54 +0330

    not complete node.js code

--- s06.js
+++ s06.js
@@ -1,15 +1,41 @@
 let http = require("http");
+let port = 8037
 let server = http.createServer(requestHandler);
-server.listen(8037);
+server.listen(port);
+console.log("Server is running on port", port)
 
 let headers = {"Content-Type": "Text/Plain"}
 
+let obj = {
+    x : function(){
+        console.log("how are you?");
+
+        response.writeHeader(200, headers);
+        response.write("how are you?");
+        response.end();
+    },
+    y : function(){
+        console.log("what are you doing?");
+
+        response.writeHeader(200, headers);
+        response.write("what are you doing?");
+        response.end();
+    },
+    "favicon.ico": function(){
+        console.log("favicon")
+
+        response.writeHeader(200, headers);
+        response.write("Hello");
+        response.end();
+    }
+}
 function requestHandler(request, response){
-    console.log("request-url: ",request.url);
+
+    console.log(request.url.split("/")[1]);
     console.log("request-method: ", request.method)
 
-    console.log("request recived: ", request.url);
     response.writeHeader(200, headers);
-    response.write("Hello");
-    response.end();
+        response.write("Hello");
+        response.end();
+
 }
